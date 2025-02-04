dd3cbcd582832a5e1781dac2215f4f9f11dc380b
gozica <109421350+gozica@users.noreply.github.com>
2025-02-04 18:54:05 -0500

    iterables

---
+++ .venv/Lib/iterables.py
@@ -0,0 +1,20 @@
+#Iterables = An object/collection that can return its elements one at a time,
+# allowing it to be iterated over in a loop
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+    #sets
+fruits = {"apple", "orange", "banana"}
+
+for fruit in fruits:
+    print(fruit)
+
+#string
+
+name = "nellz g"
+
+for character in name:
+    print(character, end="")
